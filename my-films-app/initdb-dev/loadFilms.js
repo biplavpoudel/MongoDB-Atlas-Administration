4baf06ba9e12852bce61b2d37f14693343a6fd4e
Biplav Poudel <biplavpoudel764@gmail.com>
2025-12-11 13:00:46 +0545

    initial commit

---
+++ my-films-app/initdb-dev/loadFilms.js
@@ -0,0 +1,16 @@
+db.getSiblingDB('my-films-db').getCollection('films').insertMany([
+    {
+       title: 'Titanic',
+       year: 1997,
+       genres: [ 'Drama', 'Romance' ]
+    },
+    {
+       title: 'Spirited Away',
+       year: 2001,
+       genres: [ 'Animation', 'Adventure', 'Family' ]
+    },
+    {
+       title: 'Casablanca',
+       genres: [ 'Drama', 'Romance', 'War' ]
+    }
+ ] );
